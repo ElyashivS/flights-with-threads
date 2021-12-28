d3e2aa3392f5540e1d963b68da7ce97576a02417
Elyashiv <Elyashiv2468@gmail.com>
2021-12-28 17:19:16 +0200

    Progress

--- src/AirPort.java
+++ src/AirPort.java
@@ -1,29 +1,56 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class AirPort {
-    private String name;
-    private int numOfRunways;
 
-    public AirPort(String name, int numOfRunways) {
+    private final String name;
+    boolean[] freeRunWays;
+    Queue<Integer> queue = new LinkedList<>();
+
+    public AirPort(String name, int numOfRunway) {
         this.name = name;
-        this.numOfRunways = numOfRunways;
+        freeRunWays = new boolean[numOfRunway];
+        Arrays.fill(freeRunWays, true); // Make all values true;
     }
 
-    public int depart(int flightNum) {
-        return 0;
+    public synchronized int depart(int flightNum) {
+        return waitForFreeRunway(flightNum);
     }
 
-    public int land(int flightNum) {
-        return 0;
+    public synchronized int land(int flightNum) {
+        return waitForFreeRunway(flightNum);
     }
 
-    public void freeRunways(int flightNum) {
+    private synchronized int waitForFreeRunway(int flightNum) {
+        int temp = getFreeRunway();
+        while (temp == -1) {
+            try {
+                wait();
+                temp = getFreeRunway();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        queue.add(flightNum);
+        return temp;
+    }
 
+    public synchronized void makeFreeRunway(int flightNum, int numOfFreeRunway) {
+        // What do I do with the flightNum?
+        freeRunWays[numOfFreeRunway] = true;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getNumOfRunways() {
-        return numOfRunways;
+    private synchronized int getFreeRunway() {
+        for (int i = 0; i < freeRunWays.length; i++) {
+            if (freeRunWays[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
