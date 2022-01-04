70a066931098e15047430830c73f1ea8d0d99289
Elyashiv <Elyashiv2468@gmail.com>
2022-01-04 10:41:29 +0200

    Added some comments. Done.

--- src/AirPort.java
+++ src/AirPort.java
@@ -5,13 +5,23 @@ import java.util.Queue;
 public class AirPort {
 
     private final String name;
-    boolean[] freeRunWays;
-    Queue<Integer> queue = new LinkedList<>();
+    private boolean[] freeRunWays;
+    private Queue<Integer> queue = new LinkedList<>();
 
+    // Constructor
     public AirPort(String name, int numOfRunway) {
         this.name = name;
         freeRunWays = new boolean[numOfRunway];
         Arrays.fill(freeRunWays, true); // Make all values true;
+    }
+
+    // Getters
+    public Queue<Integer> getQueue() {
+        return queue;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public synchronized int depart(int flightNum) {
@@ -22,33 +32,44 @@ public class AirPort {
         return waitForFreeRunway(flightNum);
     }
 
+    /**
+     * This function make the thread wait if there is no free runway, or if it's not his turn according
+     * to the queue.
+     * @param flightNum the number of flight.
+     * @return free runway to depart or land.
+     */
     private synchronized int waitForFreeRunway(int flightNum) {
-        // No available runway, or not his turn. Exit from while when find free runway and his turn.
+        // No available runway, or not his turn. Exit from loop when find free runway and his turn.
         while ((getFreeRunway() == -1) || (queue.peek() != null && queue.peek() != flightNum)) {
-//            System.out.println((flightNum + 1) + " get route: " + temp);
             try {
-                wait(); // notify in line 46
+                wait(); // Notify in the function makeFreeRunway.
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
         }
         int temp = getFreeRunway(); // Trying again.
+
         // Found free runway and his turn.
         freeRunWays[temp] = false;
         queue.poll();
         return temp;
     }
 
-    public synchronized void makeFreeRunway(int flightNum, int numOfFreeRunway) {
+    /**
+     * This function free the runway that has been taken.
+     * @param numOfFreeRunway number of the runway to make free.
+     */
+    // This function should be named "freeRunway" according to the assignment
+    public synchronized void makeFreeRunway(int numOfFreeRunway) {
         freeRunWays[numOfFreeRunway] = true;
         notifyAll();
     }
 
-    public String getName() {
-        return name;
-    }
-
+    /**
+     * This function get free runway from the airport.
+     * @return the number of next free runway.
+     */
     private synchronized int getFreeRunway() {
         for (int i = 0; i < freeRunWays.length; i++) {
             if (freeRunWays[i]) {
--- src/Flight.java
+++ src/Flight.java
@@ -1,14 +1,17 @@
-public class Flight extends Thread{
+public class Flight extends Thread {
+
     private final int flightNum;
     private final AirPort departAirPort;
     private final AirPort landAirPort;
 
+    // Constructor
     public Flight(int flightNum, AirPort departAirPort, AirPort landAirPort) {
         this.flightNum = flightNum;
         this.departAirPort = departAirPort;
         this.landAirPort = landAirPort;
     }
 
+    // Getters
     public int getFlightNum() {
         return flightNum;
     }
@@ -28,9 +31,8 @@ public class Flight extends Thread{
         long randTime = 2000 + (long) (Math.random() * (3000)); // Random seconds between 2 and 5.
         long randTimeOfFlying = 4000 + (long) (Math.random() * (5000)); // Random seconds between 4 and 7.
 
-
         // Departing
-        departAirPort.queue.add(flightNum); // Add flight to queue
+        departAirPort.getQueue().add(flightNum); // Add flight to queue
         int freeRunwayToDepart = departAirPort.depart(flightNum); // Trying to get number of free runway to depart from.
         try { // Got number of freeway, and his turn. DEPARTING.
             sleep(200);
@@ -40,7 +42,7 @@ public class Flight extends Thread{
             e.printStackTrace();
         }
 
-        departAirPort.makeFreeRunway(flightNum, freeRunwayToDepart);
+        departAirPort.makeFreeRunway(freeRunwayToDepart);
         System.out.println("runway number " + (freeRunwayToDepart + 1) + " in " + departAirPort.getName() + " is clear");
 
         // Flying
@@ -51,7 +53,7 @@ public class Flight extends Thread{
         }
 
         // Landing
-        landAirPort.queue.add(flightNum); // Add flight to queue
+        landAirPort.getQueue().add(flightNum); // Add flight to queue
         int freeRunwayToLand = landAirPort.land(flightNum);
         try { // Got number of freeway, and his turn. LANDING.
             sleep(200);
@@ -60,7 +62,7 @@ public class Flight extends Thread{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        landAirPort.makeFreeRunway(flightNum, freeRunwayToLand);
+        landAirPort.makeFreeRunway(freeRunwayToLand);
         System.out.println("runway number " + (freeRunwayToDepart + 1) + " in " + departAirPort.getName() + " is clear");
     }
 }
